e3465d16f8fbbcf6f19255b9b1dd7eb2bb9d0431
Robert Burek <robertburek@wp.pl>
2020-05-08 22:06:15 +0200

    Czytanie danych z URL - działa
      do innej tabeli, ale działa, teraz biorę się za widoki
      przenisę tabelę do innej bazy i tam podziałam

--- src/main/java/pl/info/mojeakcje/maestroameryka/CzytanieDanychJsoup.java
+++ src/main/java/pl/info/mojeakcje/maestroameryka/CzytanieDanychJsoup.java
@@ -29,13 +29,13 @@ public class CzytanieDanychJsoup {
 
         List<SpolkaAmeryka> nowaListaSpolek = new ArrayList<>();
 
-        Double course1M = 0.0;
-        Double course3M = 0.0;
-        Double course12M = 0.0;
-        Double courseYTD = 0.0;
-        Double courseCurrent = 0.0;
-        Double wynik = 0.0;
-        String stopa = "";
+        Double course1M;
+        Double course3M;
+        Double course12M;
+        Double courseYTD;
+        Double courseCurrent;
+        Double wynik;
+        String stopa;
 
         // ustalanie parametrów dla URL
         LocalDate localDateStart = LocalDate.of(2020, 05, 02);
@@ -50,6 +50,17 @@ public class CzytanieDanychJsoup {
         LocalDate localDateFind = LocalDate.now();
 
         for (AmerykaSpolka amSp : staraListaSpolek) {
+
+            System.out.println("");
+            System.out.println("Czytam spółkę: " + amSp.getTicker());
+
+            course1M = null;
+            course3M = null;
+            course12M = null;
+            courseYTD = null;
+            courseCurrent = null;
+            wynik = null;
+            stopa = null;
 
             // Tworzenie nowej spólki  - konstruktor (String ticker, String name, String market, String sector, String industry, String note)
             SpolkaAmeryka nowaSpolka = new SpolkaAmeryka(amSp.getTicker(), amSp.getName(), amSp.getMarket(), amSp.getSector(), amSp.getIndustry(), amSp.getNote());
@@ -130,74 +141,86 @@ public class CzytanieDanychJsoup {
 //            System.out.println(dane);
                     if (dane.startsWith(listaDat.get(0))) {
                         result = dane.split(",");
-                        courseCurrent = Double.parseDouble(result[4]);
-                        nowaSpolka.setCourseCurrent(new DecimalFormat("# 0.000").format(courseCurrent));
+                        if ((result[4] != null) && (!result[4].equals("null"))) {
+                            courseCurrent = Double.parseDouble(result[4]);
+                            nowaSpolka.setCourseCurrent(new DecimalFormat("# 0.000").format(courseCurrent));
+                        }
                         nowaSpolka.setDayCourseCurrent(result[0]);
-//                        System.out.println("courseCurrent " + nowaSpolka.getDayCourseCurrent() + " - " + courseCurrent);
                         System.out.println("courseCurrent " + nowaSpolka.getDayCourseCurrent() + " - " + nowaSpolka.getCourseCurrent());
                     }
                     if (dane.startsWith(listaDat.get(1))) {
                         result = dane.split(",");
-                        course12M = Double.parseDouble(result[4]);
-                        nowaSpolka.setCourse12M(new DecimalFormat("# 0.000").format(course12M));
+                        if ((result[4] != null) && (!result[4].equals("null"))) {
+                            course12M = Double.parseDouble(result[4]);
+                            nowaSpolka.setCourse12M(new DecimalFormat("# 0.000").format(course12M));
+                        }
                         nowaSpolka.setDay12M(result[0]);
-//                        System.out.println("course12M " + nowaSpolka.getDay12M() + " - " + course12M);
                         System.out.println("course12M " + nowaSpolka.getDay12M() + " - " + nowaSpolka.getCourse12M());
                     }
                     if (dane.startsWith(listaDat.get(2))) {
                         result = dane.split(",");
-                        courseYTD = Double.parseDouble(result[4]);
-                        nowaSpolka.setCourseYTD(new DecimalFormat("# 0.000").format(courseYTD));
+                        if ((result[4] != null) && (!result[4].equals("null"))) {
+                            courseYTD = Double.parseDouble(result[4]);
+                            nowaSpolka.setCourseYTD(new DecimalFormat("# 0.000").format(courseYTD));
+                        }
                         nowaSpolka.setDayYTD(result[0]);
-//                        System.out.println("courseYTD " + nowaSpolka.getDayYTD() + " - " + courseYTD);
                         System.out.println("courseYTD " + nowaSpolka.getDayYTD() + " - " + nowaSpolka.getCourseYTD());
                     }
                     if (dane.startsWith(listaDat.get(3))) {
                         result = dane.split(",");
-                        course3M = Double.parseDouble(result[4]);
-                        nowaSpolka.setCourse3M(new DecimalFormat("# 0.000").format(course3M));
+                        if ((result[4] != null) && (!result[4].equals("null"))) {
+                            course3M = Double.parseDouble(result[4]);
+                            nowaSpolka.setCourse3M(new DecimalFormat("# 0.000").format(course3M));
+                        }
                         nowaSpolka.setDay3M(result[0]);
-//                        System.out.println("course3M " + nowaSpolka.getDay3M() + " - " + course3M);
                         System.out.println("course3M " + nowaSpolka.getDay3M() + " - " + nowaSpolka.getCourse3M());
                     }
                     if (dane.startsWith(listaDat.get(4))) {
                         result = dane.split(",");
-                        course1M = Double.parseDouble(result[4]);
-                        nowaSpolka.setCourse1M(new DecimalFormat("# 0.000").format(course1M));
+                        if ((result[4] != null) && (!result[4].equals("null"))) {
+                            course1M = Double.parseDouble(result[4]);
+                            nowaSpolka.setCourse1M(new DecimalFormat("# 0.000").format(course1M));
+                        }
                         nowaSpolka.setDay1M(result[0]);
-//                        System.out.println("course1M " + nowaSpolka.getDay1M() + " - " + course1M);
                         System.out.println("course1M " + nowaSpolka.getDay1M() + " - " + nowaSpolka.getCourse1M());
                     }
                 }
 
 
                 // wyliczanie stóp zwrotu dla listy dat
-                wynik = courseCurrent * 100 / course1M - 1;
-                stopa = new DecimalFormat("0.0").format(wynik);
-                nowaSpolka.setM1(stopa + "%");
-                System.out.println("M1: " + nowaSpolka.getM1());
-
-                wynik = courseCurrent * 100 / course3M - 1;
-                stopa = new DecimalFormat("0.0").format(wynik);
-                nowaSpolka.setM3(stopa + "%");
-                System.out.println("M3: " + nowaSpolka.getM3());
-
-                wynik = courseCurrent * 100 / course12M - 1;
-                stopa = new DecimalFormat("0.0").format(wynik);
-                nowaSpolka.setM12(stopa + "%");
-                System.out.println("M12: " + nowaSpolka.getM12());
-
-                wynik = courseCurrent * 100 / courseYTD - 1;
-                stopa = new DecimalFormat("0.0").format(wynik);
-                nowaSpolka.setyTD(stopa + "%");
-                System.out.println("YTD: " + nowaSpolka.getyTD());
+                if ((courseCurrent != null) && (!courseCurrent.equals("null"))) {
+                    if (course1M != null) {
+                        wynik = ((courseCurrent / course1M) - 1) * 100;
+                        stopa = new DecimalFormat("0.0").format(wynik);
+                        nowaSpolka.setM1(stopa + "%");
+                        System.out.println("M1: " + nowaSpolka.getM1());
+                    }
+                    if (course3M != null) {
+                        wynik = ((courseCurrent / course3M) - 1) * 100;
+                        stopa = new DecimalFormat("0.0").format(wynik);
+                        nowaSpolka.setM3(stopa + "%");
+                        System.out.println("M3: " + nowaSpolka.getM3());
+                    }
+                    if (course12M != null) {
+                        wynik = ((courseCurrent / course12M) - 1) * 100;
+                        stopa = new DecimalFormat("0.0").format(wynik);
+                        nowaSpolka.setM12(stopa + "%");
+                        System.out.println("M12: " + nowaSpolka.getM12());
+                    }
+                    if (courseYTD != null) {
+                        wynik = ((courseCurrent / courseYTD) - 1) * 100;
+                        stopa = new DecimalFormat("0.0").format(wynik);
+                        nowaSpolka.setyTD(stopa + "%");
+                        System.out.println("YTD: " + nowaSpolka.getyTD());
+                    }
+                }
 
             } // warunek nie pustej listy dat
 
             nowaSpolka = changeNull(nowaSpolka);
             nowaListaSpolek.add(nowaSpolka);
 
-            int millis = new Random().nextInt(800) + 700;
+            int millis = new Random().nextInt(400) + 300;
             System.out.println("Czekam: " + millis + "ms");
             Thread.sleep(millis);
 
