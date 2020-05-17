a32c88150210839e23fd1449c03a12b870f209e8
Robert Burek <robertburek@wp.pl>
2020-05-17 15:44:08 +0200

    Przygotowanie do zbierania informacji o gościach cd
      Controller

---
+++ src/main/java/pl/info/mojeakcje/maestroameryka/controller/GoscieController.java
@@ -0,0 +1,23 @@
+package pl.info.mojeakcje.maestroameryka.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static pl.info.mojeakcje.maestroameryka.MaestroamerykaApplication.ANSI_RED;
+import static pl.info.mojeakcje.maestroameryka.MaestroamerykaApplication.ANSI_RESET;
+
+@Log4j2
+@Controller
+public class GoscieController {
+
+    protected final org.slf4j.Logger log = LoggerFactory.getLogger(getClass());
+
+    @GetMapping("/info")
+    public void infoPage(Model model) {
+        log.info(ANSI_RED + "Jestem w info. " + ANSI_RESET);
+//        return "amerykawidok";
+    }
+}
