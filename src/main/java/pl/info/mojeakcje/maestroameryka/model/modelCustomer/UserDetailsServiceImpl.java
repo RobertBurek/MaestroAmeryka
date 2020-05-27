8648dcd8e3555dada7de3bcd49f5b5aa14ef502f
Robert Burek <robertburek@wp.pl>
2020-05-27 22:42:30 +0200

    Całkowity refacktor UserDetailsServiceImpl

--- src/main/java/pl/info/mojeakcje/maestroameryka/model/modelCustomer/UserDetailsServiceImpl.java
+++ src/main/java/pl/info/mojeakcje/maestroameryka/model/modelCustomer/UserDetailsServiceImpl.java
@@ -2,10 +2,6 @@ package pl.info.mojeakcje.maestroameryka.model.modelCustomer;
 
 import lombok.extern.log4j.Log4j2;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -36,20 +32,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         this.showSpolka = showSpolka;
     }
 
-//    @Autowired
-//    public UserDetailsServiceImpl(CustoRepository custoRepository) {
-//        this.custoRepository = custoRepository;
-//    }
-
-
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-
-
-
-
-
         log.info(ANSI_MORSKI + ANSI_BOLD + "Próba logowania przez: " + ANSI_BLUEE + s + ANSI_RESET);
         UserDetails byNickCustomer = custoRepository.findByNickCustomer(s);
         UserDetails userDetailsNew;
@@ -62,18 +47,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                     .build();
             byNickCustomer = userDetailsNew;
         }
-        if (!byNickCustomer.getUsername().equals("Guest")){
-            currentUser.setName(byNickCustomer.getUsername());
-            queryRepository.findAllWszystkieDane(currentUser.getName());
-            showSpolka.setShow(false);
-        } else {
-        currentUser.setName("Guest");
-        queryRepository.findAllWszystkieDane("Guest");
-        showSpolka.setShow(false);
-        }
-//            log.info("" + byNickCustomer.getAuthorities().size());
-//            log.info(byNickCustomer.getPassword());
-//            log.info(byNickCustomer.getUsername());
         return byNickCustomer;
     }
 
